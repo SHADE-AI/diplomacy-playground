69ccb8bf3ce890c31620bbfde6ca74661c8d195f
vaughn <vaughn@tacc.utexas.edu>
2022-02-07 15:26:41 -0600

    Fixed merge conflict

---
+++ systemd/README.md
@@ -0,0 +1,5 @@
+## Requirements
+
+1. Docker CE must be installed and active as a systemd service
+2. Logrotate must be installed to handle the server and webui logs
+3. Port 8432 and Ports 8434-8700 must be accessible to external connections
--- systemd/install.sh
+++ systemd/install.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-# set -ex
-
+# Create user
 useradd -m diplomacy &&
     usermod -aG docker diplomacy
 
+# Create game data and log directories
 mkdir -p /home/diplomacy/data &&
     mkdir -p /home/diplomacy/logs &&
     chown -R diplomacy:diplomacy /home/diplomacy
 
+# Install systemd units
 cp docker.diplomacy_server.service /etc/systemd/system &&
     cp docker.diplomacy_webui.service /etc/systemd/system
 
+# Enable servers
 systemctl daemon-reload &&
     systemctl enable docker.diplomacy_server &&
     systemctl enable docker.diplomacy_webui
 
-# set +ex
+# Logrotate configurations
+cp diplomacy_server /etc/logrotate.d/ &&
+    cp diplomacy_webui /etc/logrotate.d/
