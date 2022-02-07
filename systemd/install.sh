cccf81e5745053a14c6ad649497f3e71091b9182
Matthew Vaughn <vaughn@tacc.utexas.edu>
2022-02-07 13:11:34 -0600

    Working systemd units and installer script for game and webui server.

--- systemd/install.sh
+++ systemd/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+# set -ex
 
 useradd -m diplomacy &&
     usermod -aG docker diplomacy
@@ -9,11 +9,11 @@ mkdir -p /home/diplomacy/data &&
     mkdir -p /home/diplomacy/logs &&
     chown -R diplomacy:diplomacy /home/diplomacy
 
-cp docker.diplomacy_server /etc/systemd/system &&
-    cp docker.diplomacy_webui /etc/systemd/system
+cp docker.diplomacy_server.service /etc/systemd/system &&
+    cp docker.diplomacy_webui.service /etc/systemd/system
 
 systemctl daemon-reload &&
     systemctl enable docker.diplomacy_server &&
     systemctl enable docker.diplomacy_webui
 
-set +ex
+# set +ex
